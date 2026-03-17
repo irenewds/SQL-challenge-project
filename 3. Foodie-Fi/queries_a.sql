dad71ed52c7b6b361d09b96ed4094f78068dd7a6
Irene Widyastuti <widyasirene@gmail.com>
2026-03-17 19:48:35 +0700

    adding more

---
+++ 3. Foodie-Fi/queries_a.sql
@@ -0,0 +1,5 @@
+USE foodie_fi;
+
+-- A. Customer Journey
+-- Based off the 8 sample customers provided in the sample from the subscriptions table, write a brief description about each customer’s onboarding journey.
+-- Try to keep it as short as possible - you may also want to run some sort of join to make your explanations a bit easier!
