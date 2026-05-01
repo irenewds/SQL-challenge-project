4909626dccc2ebcfad4b2c7593431d1f33058450
Irene Widyastuti <widyasirene@gmail.com>
2026-05-01 17:49:36 +0700

    adding analysis

--- 4. Data Bank/queries_a.sql
+++ 4. Data Bank/queries_a.sql
@@ -1,12 +1,38 @@
 USE data_bank;
 
+SELECT *
+FROM regions;
+SELECT *
+FROM customer_nodes
+LIMIT 10;
+SELECT *
+FROM customer_transactions
+LIMIT 10;
+
+
 -- A. Customer Nodes Exploration
 
 -- 1. How many unique nodes are there on the Data Bank system?
+SELECT COUNT(DISTINCT node_id) AS unique_node
+FROM customer_nodes;
 
 -- 2. What is the number of nodes per region?
+SELECT
+    r.region_name AS region,
+    COUNT(DISTINCT cn.node_id) AS number_of_nodes
+FROM customer_nodes cn
+JOIN regions r
+    ON cn.region_id = r.region_id
+GROUP BY region;
 
 -- 3. How many customers are allocated to each region?
+SELECT
+    r.region_name AS region,
+    COUNT(DISTINCT cn.customer_id) AS total_customers
+FROM customer_nodes cn
+JOIN regions r
+    ON cn.region_id = r.region_id
+GROUP BY region;
 
 -- 4. How many days on average are customers reallocated to a different node?
 
--- 4. Data Bank/queries_b.sql
+++ 4. Data Bank/queries_b.sql
@@ -1,10 +1,39 @@
 USE data_bank;
 
+SELECT *
+FROM regions;
+SELECT *
+FROM customer_nodes
+LIMIT 10;
+SELECT *
+FROM customer_transactions
+LIMIT 10;
+
 -- B. Customer Transactions
 
 -- 1. What is the unique count and total amount for each transaction type?
+SELECT
+    txn_type,
+    COUNT(*) AS unique_count,
+    SUM(txn_amount) AS total_amount
+FROM customer_transactions
+GROUP BY txn_type;
 
 -- 2. What is the average total historical deposit counts and amounts for all customers?
+WITH deposits AS (
+SELECT
+    customer_id,
+    COUNT(*) AS count,
+    SUM(txn_amount) AS sum
+FROM customer_transactions
+WHERE txn_type = 'deposit'
+GROUP BY customer_id
+)
+
+SELECT
+    ROUND(AVG(count)) AS average_count,
+    ROUND(AVG(sum)) AS average_amount
+FROM deposits;
 
 -- 3. For each month - how many Data Bank customers make more than 1 deposit and either 1 purchase or 1 withdrawal in a single month?
 
