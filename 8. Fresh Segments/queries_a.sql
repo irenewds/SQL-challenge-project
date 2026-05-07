ff4b02a644cec707e287d5de18a60feec386d87d
Irene Widyastuti <widyasirene@gmail.com>
2026-05-07 15:07:01 +0700

    data exploration

--- 8. Fresh Segments/queries_a.sql
+++ 8. Fresh Segments/queries_a.sql
@@ -1,12 +1,5 @@
 USE fresh_segments;
 
-SELECT *
-FROM interest_map
-LIMIT 10;
-SELECT *
-FROM interest_metrics
-LIMIT 10;
-
 -- A. Data Exploration and Cleansing
 
 -- 1. Update the fresh_segments.interest_metrics table by modifying the month_year column to be a date data type with the start of the month.
