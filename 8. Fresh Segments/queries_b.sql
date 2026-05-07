41548ebbba91eb0b24d989ec410dada4fdc7baa3
Irene Widyastuti <widyasirene@gmail.com>
2026-05-07 15:15:51 +0700

    interest analysis

--- 8. Fresh Segments/queries_b.sql
+++ 8. Fresh Segments/queries_b.sql
@@ -1,8 +1,31 @@
 USE fresh_segments;
 
+SELECT *
+FROM interest_map
+LIMIT 10;
+SELECT *
+FROM interest_metrics
+LIMIT 10;
+
 -- B. Interest Analysis
 
 -- 1. Which interests have been present in all month_year dates in our dataset?
+WITH total_months AS (
+    SELECT COUNT(DISTINCT month_year) AS month_count
+    FROM fresh_segments.interest_metrics
+)
+
+SELECT
+    im.interest_id,
+    mp.interest_name,
+    COUNT(DISTINCT im.month_year) AS months_present
+FROM fresh_segments.interest_metrics im
+INNER JOIN fresh_segments.interest_map mp
+    ON im.interest_id = mp.id
+WHERE im.month_year IS NOT NULL
+GROUP BY im.interest_id, mp.interest_name
+HAVING COUNT(DISTINCT im.month_year) = (SELECT month_count FROM total_months)
+ORDER BY im.interest_id;
 
 -- 2. Using this same total_months measure - calculate the cumulative percentage of all records starting at 14 months - which total_months value passes the 90% cumulative percentage value?
 
